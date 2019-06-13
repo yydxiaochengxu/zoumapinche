977949c4d5a582b7d2ca6a8a302f033ace4c3025
yydxiaochengxu <553587917@qq.com>
2019-06-13 23:55:58 +0800

    dev1提交版本

--- remove.js
+++ remove.js
@@ -6,5 +6,11 @@ function aa(){
     return function(){
         console.log('新加dev匿名函数')
         console.log('新加dev匿名函数')
+        (function(){
+            console.log('我是div1');
+            setTimeout(()=>{
+                alert('div1')
+            },1000)
+        }())
     }
 }
