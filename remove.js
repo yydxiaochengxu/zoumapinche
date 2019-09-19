a1943b69d75e11d9a3458d382788e535f24b6b03
yydxiaochengxu <553587917@qq.com>
2019-09-19 21:29:17 +0800

    xiugai

--- remove.js
+++ remove.js
@@ -1,16 +1,16 @@
-function aa(){
-    function add(){
+function aa() {
+    function add() {
         console.log('master冲突函数')
     }
     add()
-    return function(){
-        console.log('新加dev匿名函数')
+    return function () {
+        console.log('新加dev匿名函数111')
         console.log('新加dev匿名tianjia函数')
-        (function(){
-            console.log('我是div1');
-            setTimeout(()=>{
-                alert('div1')
-            },1000)
-        }())
+            (function () {
+                console.log('我是div1');
+                setTimeout(() => {
+                    alert('div1')
+                }, 1000)
+            }())
     }
 }
