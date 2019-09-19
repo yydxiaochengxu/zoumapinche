b725bca8f5275d7235bde40e4e2963db0a602fa6
yydxiaochengxu <553587917@qq.com>
2019-09-19 23:27:16 +0800

    本地修改

--- remove.js
+++ remove.js
@@ -4,16 +4,50 @@ function aa() {
     }
     add()
     return function () {
-        console.log('新加dev匿名函数111')
-        console.log('新加dev匿名tianjia函数')
-            (function () {
-                console.log('我是div1');
-                setTimeout(() => {
-                    console.warn('div1')
-                }, 1000)
-            }())
+        console.log('新加dev匿名tianjia函数');
+        (function () {
+            console.log('我是div1');
+            setTimeout(() => {
+                console.log('div1')
+            }, 1000)
+        }())
     }
 }
-function loop(){
-    console.log('loop')
+function createStore(reducer) {
+    let store, observer = {};
+    let gerState = () => store;
+    let dispatch = (action) => {
+        store = reducer(store, action);
+        if (Array.isArray(observer[action.type])) {
+            observer[action.type].forEach(cb => cb())
+        }
+    }
+    let subscribe = (subCbObj) => {
+        let { type, cb } = subCbObj;
+        if (Array.isArray(observer[type])) {
+            observer[type].push(cb)
+        } else {
+            observer[type] = [cb]
+        }
+        return () => {
+            observer[type] = observer[type].filter(item => item !== cb)
+        }
+    }
+    return {
+        gerState, dispatch, subscribe
+    }
+}
+let reducer = (state = {}, action) => {
+    let { type, data } = action
+    switch (type) {
+        case 'add': {
+            return Object.assign({}, state, data)
+        }
+        default:
+            return state
+    }
+}
+let store = createStore(reducer)
+module.exports = {
+    store
 }
