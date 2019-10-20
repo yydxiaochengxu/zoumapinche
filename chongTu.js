7068b481ab5ce6fbb158be8ddc832f1a390afeca
xiaodengyyd <1358776709@qq.com>
2019-10-20 22:29:19 +0800

    修改aa函数

--- chongTu.js
+++ chongTu.js
@@ -1,20 +1,3 @@
-function aa() {
-    function add() {
-        console.log('master冲突函数')
-    }
-    add()
-    return function () {
-        alert('新加dev匿名函数111');
-        console.log('新加dev匿名tianjia函数');
-        (function () {
-            console.log('我是div1');
-            setTimeout(() => {
-                console.log('div1')
-            }, 1000)
-        }())
-    }
-}
-alert('改变')
 function createStore(reducer) {
     let store, observer = {};
     let gerState = () => store;
@@ -53,17 +36,58 @@ let store = createStore(reducer)
 module.exports = {
     store
 }
+
+
+
+
+function createStore(reducer) {
+    let store, observer = {};
+    let gerState = () => store;
+    let dispatch = (action) => {
+        store = reducer(store, action);
+        if (Array.isArray(observer[action.type])) {
+            observer[action.type].forEach(cb => cb())
+        }
+    }
+    let subscribe = (subCbObj) => {
+        let { type, cb } = subCbObj;
+        if (Array.isArray(observer[type])) {
+            observer[type].push(cb)
+        } else {
+            observer[type] = [cb]
+        }
+        return () => {
+            observer[type] = observer[type].filter(item => item !== cb)
+        }
+    }
+    return {
+        gerState, dispatch, subscribe
+    }
+}
+let reducer = (state = {}, action) => {
+    let { type, data } = action
+    switch (type) {
+        case 'add': {
+            return Object.assign({}, state, data)
+        }
+        default:
+            return state
+    }
+}
+let store = createStore(reducer)
+module.exports = {
+    store
+}
+
+
+
 
 
 
 // 本地：
-function aa() {
-    function add() {
-        console.log('master冲突函数')
-    }
-    add()
+function aalocation() {
     return function () {
-        alert('新加dev匿名函数111');
+        alert('新加dev匿名函数');
         console.log('新加dev匿名tianjia函数');
         (function () {
             console.log('我是div1');
@@ -73,48 +97,6 @@ function aa() {
         }())
     }
 }
-function createStore(reducer) {
-    let store, observer = {};
-    let gerState = () => store;
-    let dispatch = (action) => {
-        store = reducer(store, action);
-        if (Array.isArray(observer[action.type])) {
-            observer[action.type].forEach(cb => cb())
-        }
-    }
-    let subscribe = (subCbObj) => {
-        let { type, cb } = subCbObj;
-        if (Array.isArray(observer[type])) {
-            observer[type].push(cb)
-        } else {
-            observer[type] = [cb]
-        }
-        return () => {
-            observer[type] = observer[type].filter(item => item !== cb)
-        }
-    }
-    return {
-        gerState, dispatch, subscribe
-    }
-}
-let reducer = (state = {}, action) => {
-    let { type, data } = action
-    switch (type) {
-        case 'add': {
-            return Object.assign({}, state, data)
-        }
-        default:
-            return state
-    }
-}
-let store = createStore(reducer)
-module.exports = {
-    store
-}
-
-
-
-
 // 远程：
 function aa() {
     function add() {
